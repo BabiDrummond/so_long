2611747af5011deed6463630eaeec8d2ee295470
bmoreira <bmoreira@student.42sp.org.br>
2025-11-06 22:40:06 -0300

    finished rendering. wip random collectibles

--- src/graphics/render_game.c
+++ src/graphics/render_game.c
@@ -6,104 +6,79 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 21:52:29 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/11/06 21:38:37 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/11/06 22:39:33 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static void	render_environment(t_game *game)
+static void	render_environment(t_game *game, int col, int row, char tile)
 {
-	int	row;
-	int	col;
-
-	row = -1;
-	while (++row < game->map.rows)
-	{
-		col = -1;
-		while (++col < game->map.cols)
-		{
-			if (game->map.grid[row][col] == '0')
-				mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-					game->img[FLOOR], col * TILE, row * TILE);
-			if ((game->map.grid[row][col] == '1') && (row == 0 && col == 1))
-					mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-						game->img[WALL2], col * TILE, row * TILE);
-			if ((game->map.grid[row][col] == '1') && (row != 0 || col != 1))
-					mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-						game->img[WALL], col * TILE, row * TILE);
-			if (game->map.grid[row][col] == 'E')
-				mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-					game->img[FRID2], col * TILE, row * TILE);
-		}
-	}
+	if (tile == '0')
+		mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+			game->img[FLOOR], col * TILE, row * TILE);
+	if (tile == '1' && (row == 0 && col == 1))
+			mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+				game->img[WALL2], col * TILE, row * TILE);
+	if (tile == '1' && (row != 0 || col != 1))
+			mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+				game->img[WALL], col * TILE, row * TILE);
+	if (tile == 'E')
+		mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+			game->img[FRID2], col * TILE, row * TILE);
 }
 
-static void	render_collectible(t_game *game)
+static void	render_collectible(t_game *game, int col, int row, int item)
 {
-	int	row;
-	int	col;
-	int	count;
-
-	row = -1;
-	count = 0;
-	while (++row < game->map.rows)
-	{
-		col = -1;
-		while (++col < game->map.cols)
-		{
-			if (game->map.grid[row][col] == 'C' && count++ % 3 == 0)
-				mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-					game->img[BREAD], col * TILE, row * TILE);
-			if (game->map.grid[row][col] == 'C' && count++ % 3 == 1)
-				mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-					game->img[BUTTER], col * TILE, row * TILE);
-			if (game->map.grid[row][col] == 'C' && count++ % 3 == 2)
-				mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-					game->img[MILK], col * TILE, row * TILE);
-			if (count == 2)
-				count = 0;
-		}
-	}
+	if (item % 3 == 0)
+		mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+		game->img[BREAD], col * TILE, row * TILE);
+	if (item % 3 == 1)
+		mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+		game->img[MILK], col * TILE, row * TILE);
+	if (item % 3 == 2)
+		mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+		game->img[BUTTER], col * TILE, row * TILE);
 }
 
-static void	render_player(t_game *game)
+static void	render_player(t_game *game, int col, int row)
 {
-	int	row;
-	int	col;
-
-	row = -1;
-	while (++row < game->map.rows)
-	{
-		col = -1;
-		while (++col < game->map.cols)
-			if (game->map.grid[row][col] == 'P')
-				mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-					game->img[PLAY1], col * TILE, row * TILE);
-	}
+	mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+		game->img[PLAY1], col * TILE, row * TILE);
 }
 
-static void	render_enemy(t_game *game)
+static void	render_enemy(t_game *game, int col, int row)
 {
-	int	row;
-	int	col;
-
-	row = -1;
-	while (++row < game->map.rows)
-	{
-		col = -1;
-		while (++col < game->map.cols)
-			if (game->map.grid[row][col] == 'M')
-				mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
-					game->img[CAM1], col * TILE, row * TILE);
-	}
+	mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win,
+		game->img[CAM1], col * TILE, row * TILE);
 }
 
 int	render_game(t_game *game)
 {
-	render_environment(game);
-	render_collectible(game);
-	render_player(game);
-	render_enemy(game);
+	char	tile;
+	int		item;
+	int		row;
+	int		col;
+
+	item = 1;
+	row = -1;
+	while (++row < game->map.rows)
+	{
+		col = -1;
+		while (++col < game->map.cols)
+		{
+			tile = game->map.grid[row][col];
+			if (item == 4)
+				item = 1;
+			if (tile == '0' || tile == '1' || tile == 'E')
+				render_environment(game, col, row, tile);
+			if (tile == 'C' && item++)
+				render_collectible(game, col, row, item);
+			if (tile == 'P')
+				render_player(game, col, row);
+			if (tile == 'M')
+				render_enemy(game, col, row);
+		}
+	}
 	return (0);
 }
