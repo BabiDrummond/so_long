33093dc82ca32c7cf1339c5fccafa89e02cd737e
babidrummond <babidrummond@hotmail.com>
2025-11-22 11:51:19 -0300

    fix unused variable

--- src/parsing/map_parse.c
+++ src/parsing/map_parse.c
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 19:35:24 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/11/07 19:01:11 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/11/22 11:45:06 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 void	map_parse(t_map *map, char *buffer)
 {
 	char	**split;
-	int		i;
 
-	i = 0;
 	if (!buffer)
 		error_handler(NULL, "Empty map.", EXIT_FAILURE);
 	split = ft_split(buffer, '\n');
