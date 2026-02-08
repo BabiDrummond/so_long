27ce3338b3953fed5f796033854eba4ed0c7eabf
bmoreira <bmoreira@student.42sp.org.br>
2026-02-08 00:53:59 -0300

    updated libft

--- src/validation/error_handler.c
+++ src/validation/error_handler.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map_error_handler.c                                :+:      :+:    :+:   */
+/*   error_handler.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 16:15:50 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/11/04 20:45:07 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/02/08 00:25:39 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	error_handler(char **args, const char *error_msg, int exit_code)
 {
 	if (args)
-		ft_matrix_free(args);
+		ft_split_free(args);
 	if (exit_code == EXIT_FAILURE)
 		ft_printf("%s\n", error_msg);
 	exit (exit_code);
